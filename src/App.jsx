57557eeb29c1dcaf73b12e1e517bfdb22aabf65d
mukeshsharma707 <mukeshsharma707@gmail.com>
2025-10-07 09:54:52 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h2 style={{textAlign:'center', marginBottom:'20px'}}>Counter App</h2>
+<p style={{textAlign:'center'}}>Count: {count}</p>
+<div style={{display:'flex', textAlign:'center', justifyContent:'center', flexDirection:'row', gap:'10px'}}>
+  <button onClick={()=>{
+    setCount(prev=>prev+1);
+  }}>Increment</button>
+  <button onClick={()=>setCount(prev=>prev-1)}>Decrement</button>
+</div>
+          </>
   )
 }
 
